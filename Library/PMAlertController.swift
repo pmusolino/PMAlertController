68fbe929dba53638aae37b1588f9392c023feb59
Klaus Lanzarini <klauslanza@gmail.com>
2017-03-01 12:27:22 +0100

    Add tap on background to dismiss alert.

--- Library/PMAlertController.swift
+++ Library/PMAlertController.swift
@@ -32,6 +32,7 @@ import UIKit
     open var textFields: [UITextField] = []
     
     open var gravityDismissAnimation = true
+    open var dismissWithBackgroudTouch = false // enable touch background to dismiss. Off by default.
     
     //MARK: - Lifecycle
     
@@ -68,6 +69,9 @@ import UIKit
         //if alert width = 270, else width = screen width - 36
         style == .alert ? (alertViewWidthConstraint.constant = 270) : (alertViewWidthConstraint.constant = UIScreen.main.bounds.width - 36)
         
+        let tapRecognizer: UITapGestureRecognizer = UITapGestureRecognizer.init(target: self, action: #selector(dismissAlertControllerFromBackgroundTap))
+        alertMaskBackground.addGestureRecognizer(tapRecognizer)
+        alertMaskBackground.isUserInteractionEnabled = true
         
         setShadowAlertView()
     }
@@ -94,6 +98,15 @@ import UIKit
         self.dismiss(animated: true, completion: nil)
     }
     
+    @objc fileprivate func dismissAlertControllerFromBackgroundTap() {
+        if !dismissWithBackgroudTouch {
+            return
+        }
+        
+        self.animateDismissWithGravity(.cancel)
+        self.dismiss(animated: true, completion: nil)
+    }
+
     //MARK: - Text Fields
     @objc open func addTextField(_ configuration: (_ textField: UITextField?) -> Void){
         let textField = UITextField()
