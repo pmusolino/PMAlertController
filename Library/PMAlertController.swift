c2b78e0d32a2cf1924a867a112fec9b9a9b4c07b
Oleg Gorbatchev <oleg.gorbatchev@gmail.com>
2018-09-21 01:20:33 +0200

    Updated for swift4.2 and fix warning (removed force cast)

--- Library/PMAlertController.swift
+++ Library/PMAlertController.swift
@@ -47,24 +47,22 @@ import UIKit
     
     override open func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name:NSNotification.Name.UIKeyboardWillShow, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name:NSNotification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     open override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
-        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     
     //MARK: - Initialiser
     @objc public convenience init(title: String, description: String, image: UIImage?, style: PMAlertControllerStyle) {
         self.init()
-        let nib = loadNibAlertController()
-        if nib != nil{
-            self.view = nib![0] as! UIView
-        }
+        guard let nib = loadNibAlertController(), let unwrappedView = nib[0] as? UIView else { return }
+        self.view = unwrappedView
         
         self.modalPresentationStyle = UIModalPresentationStyle.overCurrentContext
         self.modalTransitionStyle = UIModalTransitionStyle.crossDissolve
@@ -198,7 +196,7 @@ import UIKit
         keyboardHasBeenShown = true
         
         guard let userInfo = (notification as NSNotification).userInfo else {return}
-        guard let endKeyBoardFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.minY else {return}
+        guard let endKeyBoardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.minY else {return}
         
         if tempFrameOrigin == nil {
             tempFrameOrigin = alertView.frame.origin
--- PMAlertController.podspec
+++ PMAlertController.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "PMAlertController"
-  s.version      = "3.4.0"
+  s.version      = "3.5.0"
   s.summary      = "PMAlertController is a great and customizable substitute to UIAlertController"
   s.description  = <<-DESC
   					PMAlertController is a small library that allows you to substitute the uncustomizable UIAlertController of Apple, with a beautiful and totally customizable alert that you can use in your iOS app. Enjoy!
